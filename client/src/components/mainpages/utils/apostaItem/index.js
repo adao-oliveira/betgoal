f4cf95985b204d9f3df2c25870f6416c6245abc6
Adão Wellison Domingos de Oliveira <adao.domingos@ituran.com.br>
2021-10-06 13:43:52 -0300

    ajustes no formato da tabela

--- client/src/components/mainpages/utils/apostaItem/index.js
+++ client/src/components/mainpages/utils/apostaItem/index.js
@@ -10,69 +10,27 @@ function ApostaItem({ aposta, isAdmin, deleteAposta, handleCheck }) {
 
     return (
         <div className="container mt-4">
-            {/* <div className="col-md-4 col-sm-12"> */}
 
-                {
-                    isAdmin && <input type="checkbox" checked={aposta.checked}
-                        onChange={() => handleCheck(aposta._id)} />
-                }
+            {
+                isAdmin && <input type="checkbox" checked={aposta.checked}
+                    onChange={() => handleCheck(aposta._id)} />
+            }
 
-                {/* <img src={aposta.images.url} className="card-img-top img-cartao" alt="Imagem da Aposta" /> */}
+            {/* <img src={aposta.images.url} className="card-img-top img-cartao" alt="Imagem da Aposta" /> */}
 
-                {/* <div className="card" style={{ backgroundColor: "#1b8231", marginBottom: '20px' }}>
-
-                    <div className="card-body">
-                        <h5 style={{ textTransform: 'uppercase' }} titulo={aposta.titulo}>{aposta.titulo}</h5>
-                        <p className="card-text text-justify">
-                            {aposta.descricao}
-                        </p>
-
-                    </div>
-
-                    <div className="row rodape-card d-flex align-items-center text-center" style={{ margin: '10px auto' }}>
-
-                        {
-                            isAdmin ?
-                                <>
-                                    <div className="col-6">
-                                        <Link className="btn btn-sm btn-detalhes" to="#!"
-                                            onClick={() => deleteAposta(aposta._id, aposta.images.public_id)}>
-                                            Deletar
-                                        </Link>
-                                    </div>
-
-                                    <div className="col-6">
-                                        <Link className="btn btn-sm btn-detalhes" to={`/editar_aposta/${aposta._id}`}>
-                                            Editar
-                                        </Link>
-                                    </div>
-
-                                </>
-                                : <>
-                                    <div className="col-6">
-                                        <Link className="btn btn-sm btn-detalhes" to="#!" onClick={() => addCart(aposta)}>
-                                            Adicionar
-                                        </Link>
-                                    </div>
-                                    <div className="col-6">
-                                        <Link className="btn btn-sm btn-detalhes" to={`/detalhes/${aposta._id}`}>
-                                            Apostar💸
-                                        </Link>
-                                    </div>
-                                </>
-                        }
-                    </div>
-                </div>
-
-            </div> */}
-
-            <table className="table table-bordered table-striped table-dark">
+            <table class="table table-bordered table-striped table-white bg-white">
                 <thead>
                     <tr>
                         <th scope="col">
                             <h5 style={{ textTransform: 'uppercase' }} titulo={aposta.titulo}>{aposta.titulo}</h5>
                         </th>
-                        <th scope="col">
+                        {/* <th scope="col">Last</th>
+                        <th scope="col">Handle</th> */}
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td colspan="3" className="text-center">
                             <div className="btn-group">
 
                                 {
@@ -95,7 +53,7 @@ function ApostaItem({ aposta, isAdmin, deleteAposta, handleCheck }) {
                                         : <>
                                             <div className="col-6">
                                                 <Link className="btn btn-sm btn-detalhes" to="#!" onClick={() => addCart(aposta)}>
-                                                    Adicionar
+                                                <i class="fa fa-plus"></i>{aposta.taxaAposta}
                                                 </Link>
                                             </div>
                                             <div className="col-6">
@@ -106,10 +64,9 @@ function ApostaItem({ aposta, isAdmin, deleteAposta, handleCheck }) {
                                         </>
                                 }
                             </div>
-
-                        </th>
+                        </td>
                     </tr>
-                </thead>
+                </tbody>
             </table>
 
         </div>
